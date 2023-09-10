79146407714d9315d856b0606b051618657f3f27
popeyewrener <popwren@gmail.com>
2023-09-10 15:10:01 +0530

    test

--- functions/coin_function.js
+++ functions/coin_function.js
@@ -5,6 +5,7 @@ const mysql_password = process.env.DB_PASSWORD;
 const mysql_db = process.env.DB_DATABASE;
 const mysql_host = process.env.DB_HOST;
 
+const getFormattedTimestamp = require("../utils/timestamp");
 const { mysqlobject } = require("./../db_connector/mysql_connector");
 
 let coinfunction = async (req, res) => {
@@ -15,6 +16,7 @@ let coinfunction = async (req, res) => {
   let type = reqBody.type;
 
   let usertableName = "tbl_users";
+  let gametracktable = "teenpatti_transactions";
   let getdataquery = `SELECT purchased FROM ${usertableName} WHERE user_id = ${user_id}`;
 
   try {
@@ -34,23 +36,47 @@ let coinfunction = async (req, res) => {
 
     if (type == "credit") {
       let final_coins = curr_purchased + fetch_coin;
+      let logdata = {
+        "user_id":user_id,
+        "previous_balance":curr_purchased,
+        "end_balance":final_coins,
+        "amount":fetch_coin,
+        "timestamp":getFormattedTimestamp(),
+        "type":type,
+        "gameName":gamename
+    }
       let updateQuery = `UPDATE ${usertableName} SET purchased = ${final_coins} WHERE user_id = ${user_id}`;
 
       // Execute the UPDATE query using the promise-based 'query' method
       await connection.query(updateQuery);
+      await connection.query(updateQuery,logdata);
 
       res.status(200).json({ "success_id": user_id });
     } else if (type == "debit") {
       let final_coins = curr_purchased - fetch_coin;
 
       if (final_coins >= 0) {
+        let logdata = {
+            "user_id":user_id,
+            "previous_balance":curr_purchased,
+            "end_balance":final_coins,
+            "amount":fetch_coin,
+            "timestamp":getFormattedTimestamp(),
+            "type":type,
+            "gameName":gamename
+        }
         let updateQuery = `UPDATE ${usertableName} SET purchased = ${final_coins} WHERE user_id = ${user_id}`;
+        let transactionlog_query = `INSERT INTO ${gametracktable} SET ?`;
+
 
         // Execute the UPDATE query using the promise-based 'query' method
         await connection.query(updateQuery);
+        await connection.query(updateQuery,logdata);
+
+        
 
         res.status(200).json({ "success_id": user_id });
-      } else {
+      } else {          
         res.status(401).json({ "error": "not sufficient coins" });
       }
     }
---
+++ utils/timestamp.js
@@ -0,0 +1,17 @@
+let getFormattedTimestamp= () =>{
+    const now = new Date();
+  
+    const year = now.getFullYear();
+    const month = String(now.getMonth() + 1).padStart(2, '0'); // Months are zero-based
+    const day = String(now.getDate()).padStart(2, '0');
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+  
+    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+  }
+
+  module.exports = getFormattedTimestamp;
+  
+  // Output: 2023-09-09 15:33:01
+  

0f05ef4e1f71630213691e9593d34408fb70f84e
popeyewrener <popwren@gmail.com>
2023-09-10 13:14:42 +0530

    test

--- functions/coin_function.js
+++ functions/coin_function.js
@@ -6,82 +6,58 @@ const mysql_db = process.env.DB_DATABASE;
 const mysql_host = process.env.DB_HOST;
 
 const { mysqlobject } = require("./../db_connector/mysql_connector");
-let coinfunction = async (req,res)=>{
-    let reqBody = req.body;
-    //let test_num = reqBody.test_num;
-    let user_id = reqBody.id;
-    let fetch_coin = reqBody.coin;
-    let gamename = reqBody.gamename;
-    let type = reqBody.type;
 
+let coinfunction = async (req, res) => {
+  let reqBody = req.body;
+  let user_id = reqBody.id;
+  let fetch_coin = reqBody.coin;
+  let gamename = reqBody.gamename;
+  let type = reqBody.type;
 
-    //let newData = {"test_num":test_num};
-    let usertableName = "tbl_users";
-    let getdataquery = `SELECT purchased FROM ${usertableName} WHERE id = ${user_id}`
+  let usertableName = "tbl_users";
+  let getdataquery = `SELECT purchased FROM ${usertableName} WHERE id = ${user_id}`;
 
-    try {
-        const connection = await mysqlobject(mysql_host, mysql_port, mysql_username,mysql_password, mysql_db);
-        connection.query(getdataquery, (error, results) => {
-            if (error) {
-              console.error('Error inserting a new row:', error);
-              res.status(400).json({"error":error});
-            } else {
-                let userdata = results[0];
-                let curr_purchased = userdata["purchased"];
-              //console.log(`New row inserted with ID: ${results.insertId}`);
-              if (type=="credit"){
-                let final_coins = curr_purchased + fetch_coin;
-                console.error('Error inserting a new row:', error);
-                let updateQuery = `UPDATE ${usertableName} SET purchased = ${final_coins} WHERE id = ${user_id}`;
-                connection.query(updateQuery, (error,results)=>{
-                    if (error){
-                        console.error('Error inserting a new row:', error);
-                        res.status(400).json({"error":error});
+  try {
+    const connection = await mysqlobject(
+      mysql_host,
+      mysql_port,
+      mysql_username,
+      mysql_password,
+      mysql_db
+    );
 
-                    }
-                    else{
-                        res.status(200).json({"success_id": results[0].id})
+    // Execute the SELECT query using the promise-based 'query' method
+    const [results] = await connection.query(getdataquery);
 
-                    }
+    let userdata = results[0];
+    let curr_purchased = userdata["purchased"];
 
-                })
+    if (type == "credit") {
+      let final_coins = curr_purchased + fetch_coin;
+      let updateQuery = `UPDATE ${usertableName} SET purchased = ${final_coins} WHERE id = ${user_id}`;
 
-              }
-              else if (type =="debit"){
-                let final_coins = curr_purchased - fetch_coin;
-                if (final_coins >=0){
-                    let updateQuery = `UPDATE ${usertableName} SET purchased = ${final_coins} WHERE id = ${user_id}`;
-                    connection.query(updateQuery, (error, results)=>{
-                        if (error){
-                            console.error('Error inserting a new row:', error);
-                            res.status(400).json({"error":error});
+      // Execute the UPDATE query using the promise-based 'query' method
+      await connection.query(updateQuery);
 
-                        }
-                        else{
-                            res.status(200).json({"success_id": results[0].id})
+      res.status(200).json({ "success_id": user_id });
+    } else if (type == "debit") {
+      let final_coins = curr_purchased - fetch_coin;
 
-                        }
-                    })
-                }
-                else{
-                    res.status(401).json({"error":"not sufficient coins"});
-                }
+      if (final_coins >= 0) {
+        let updateQuery = `UPDATE ${usertableName} SET purchased = ${final_coins} WHERE id = ${user_id}`;
 
-              }
+        // Execute the UPDATE query using the promise-based 'query' method
+        await connection.query(updateQuery);
 
-              
-              
-              
-            }});
-        // You can use the 'connection' object for database queries here
-    
-        // Don't forget to close the connection when you're done
-        connection.end();
-      } catch (error) {
-        console.error('Error:', error);
+        res.status(200).json({ "success_id": user_id });
+      } else {
+        res.status(401).json({ "error": "not sufficient coins" });
       }
-    
-   
+    }
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(400).json({ "error": error.message });
+  }
+};
 
-}
-module.exports = {coinfunction}
+module.exports = { coinfunction };

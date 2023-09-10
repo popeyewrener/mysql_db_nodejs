4f67341b45011b77e3d9fdd5acc72a3e02695e9e
popeyewrener <popwren@gmail.com>
2023-09-10 20:10:03 +0530

    getUserPurchased socket endpoint added

--- handlers/socketHandler.js
+++ handlers/socketHandler.js
@@ -8,75 +8,42 @@ const mysql_username = process.env.DB_USERNAME;
 const mysql_password = process.env.DB_PASSWORD;
 const mysql_db = process.env.DB_DATABASE;
 const mysql_host = process.env.DB_HOST;
+let usertableName = "tbl_users";
+    let gametracktable = "teenpatti_transactions";
+    let getdataquery = `SELECT purchased FROM ${usertableName} WHERE user_id = ${user_id}`;
 handleSocket = (io,socket)=>{
     socket.on("sendMessage", (message)=>{
         io.emit("recieveMessage", message);
     });
-    socket.on("roomCreate", async (req)=>{
-        let reqBody = req;
-        reqBody = JSON.parse(reqBody);
-        const roomname = reqBody.roomname;
-        const agoraToken = reqBody.agoraToken;
-        const desc = reqBody.desc;
-        const imageUrl = reqBody.imageUrl;
-      
-        
-        const room = await createRoom(roomname,agoraToken,desc,imageUrl);
-        //console.log(userId);
-        console.log(room);
-        socket.emit("roomDetails", room)
-        
-    });
+    
     socket.on('transaction', async(data, ackCallback) => {
-        let reqBody = data;
-        reqBody = JSON.parse(reqBody);
-  let user_id = reqBody.id;
-  let fetch_coin = reqBody.coin;
-  let gamename = reqBody.gamename;
-  let type = reqBody.type;
+    let reqBody = data;
+    reqBody = JSON.parse(reqBody);
+    let user_id = reqBody.id;
+    let fetch_coin = reqBody.coin;
+    let gamename = reqBody.gamename;
+    let type = reqBody.type;
 
-  let usertableName = "tbl_users";
-  let gametracktable = "teenpatti_transactions";
-  let getdataquery = `SELECT purchased FROM ${usertableName} WHERE user_id = ${user_id}`;
+    
 
-  try {
-    let starttime = Date.now();
-    const connection = await mysqlobject(
-      mysql_host,
-      mysql_port,
-      mysql_username,
-      mysql_password,
-      mysql_db
-    );
+    try {
+        let starttime = Date.now();
+        const connection = await mysqlobject(
+        mysql_host,
+        mysql_port,
+        mysql_username,
+        mysql_password,
+        mysql_db
+        );
 
-    // Execute the SELECT query using the promise-based 'query' method
-    const [results] = await connection.query(getdataquery);
+        // Execute the SELECT query using the promise-based 'query' method
+        const [results] = await connection.query(getdataquery);
 
-    let userdata = results[0];
-    let curr_purchased = userdata["purchased"];
+        let userdata = results[0];
+        let curr_purchased = userdata["purchased"];
 
-    if (type == "credit") {
-      let final_coins = curr_purchased + fetch_coin;
-      let logdata = {
-        "user_id":user_id,
-        "previous_balance":curr_purchased,
-        "end_balance":final_coins,
-        "amount":fetch_coin,
-        "timestamp":getFormattedTimestamp(),
-        "type":type,
-        "gameName":gamename
-    }
-      let updateQuery = `UPDATE ${usertableName} SET purchased = ${final_coins} WHERE user_id = ${user_id}`;
-      let transactionlog_query = `INSERT INTO ${gametracktable} SET ?`;
-      // Execute the UPDATE query using the promise-based 'query' method
-      await connection.query(updateQuery);
-      await connection.query(transactionlog_query,logdata);
-
-      ackCallback({  "success_id": user_id });
-    } else if (type == "debit") {
-      let final_coins = curr_purchased - fetch_coin;
-
-      if (final_coins >= 0) {
+        if (type == "credit") {
+        let final_coins = curr_purchased + fetch_coin;
         let logdata = {
             "user_id":user_id,
             "previous_balance":curr_purchased,
@@ -88,31 +55,80 @@ handleSocket = (io,socket)=>{
         }
         let updateQuery = `UPDATE ${usertableName} SET purchased = ${final_coins} WHERE user_id = ${user_id}`;
         let transactionlog_query = `INSERT INTO ${gametracktable} SET ?`;
-
-
         // Execute the UPDATE query using the promise-based 'query' method
         await connection.query(updateQuery);
         await connection.query(transactionlog_query,logdata);
 
-        
+        ackCallback({  "success_id": user_id });
+        } else if (type == "debit") {
+        let final_coins = curr_purchased - fetch_coin;
 
-        ackCallback({ "success_id": user_id });
-      } else {          
-        ackCallback({ "error": "not sufficient coins" });
-      }
+        if (final_coins >= 0) {
+            let logdata = {
+                "user_id":user_id,
+                "previous_balance":curr_purchased,
+                "end_balance":final_coins,
+                "amount":fetch_coin,
+                "timestamp":getFormattedTimestamp(),
+                "type":type,
+                "gameName":gamename
+            }
+            let updateQuery = `UPDATE ${usertableName} SET purchased = ${final_coins} WHERE user_id = ${user_id}`;
+            let transactionlog_query = `INSERT INTO ${gametracktable} SET ?`;
+
+
+            // Execute the UPDATE query using the promise-based 'query' method
+            await connection.query(updateQuery);
+            await connection.query(transactionlog_query,logdata);
+
+            
+
+            ackCallback({ "success_id": user_id });
+        } else {          
+            ackCallback({ "error": "not sufficient coins" });
+        }
+        }
+        let elapsed_time = Date.now()-starttime;
+        console.log(elapsed_time);
+
+    } catch (error) {
+        console.error('Error:', error);
+        ackCallback({ "error": error.message });
     }
-    let elapsed_time = Date.now()-starttime;
-    console.log(elapsed_time);
-
-  } catch (error) {
-    console.error('Error:', error);
-    ackCallback({ "error": error.message });
-  }
-        // Process the message
+            // Process the message
     
         // Send an acknowledgment back to the client
         //ackCallback('Message received on the server!');
       });
+
+    socket.on('getUserPurchased', async(data, ackCallback)=>{
+    let reqBody = data;
+    reqBody = JSON.parse(reqBody);
+    let user_id = reqBody.id;
+    try{
+        let starttime = Date.now();
+        const connection = await mysqlobject(
+        mysql_host,
+        mysql_port,
+        mysql_username,
+        mysql_password,
+        mysql_db
+        );
+        const [results] = await connection.query(getdataquery);
+
+        let userdata = results[0];
+        let curr_purchased = userdata["purchased"];
+        ackCallback({"purchased":curr_purchased});
+
+
+    }
+    catch(e){
+        ackCallback({"error":e})
+    }
+
+
+    
+    })  
 
     
     socket.on("joinLive", (message)=>{
